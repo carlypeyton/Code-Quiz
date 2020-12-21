9ca9c77b3944ef6840a3d440e9f7e7f7f398b36b
Carly Peyton <carlypeyton@gmail.com>
2020-12-21 11:57:55 -0500

    Create function to start timer/countdown.

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ var timeLeft = document.querySelector("#time-left");
 var quizBody = document.querySelector(".quiz-body");
 var quizIntro = document.querySelector(".quiz-intro");
 
+//Function to start timer
+function startTimer() {
+    //100 seconds on clock
+    timeLeft = 100;
+    //If time left is more than 0 sec, time is shown and goes down by 1 sec
+    if (timeLeft > 0) {
+        document.getElementById("time-left").innerHTML = timeLeft;
+        timerInterval = setInterval(function () {
+            timeLeft--;
+            document.getElementById("time-left").innerHTML = timeLeft;
+            //If time runs out quiz ends
+            if (timeLeft <= 0) {
+                stopTimer();
+                endQuiz();
+                return;
+            }
+        }, 1000);
+    }
+}
