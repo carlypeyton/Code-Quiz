b8cb18f75ff2153e4df07423905949a3ba0a0f40
Carly Peyton <carlypeyton@gmail.com>
2020-12-21 10:43:27 -0500

    Add questions js. Need to add more questions to quiz.

---
+++ questions.js
@@ -0,0 +1,77 @@
+var questions = [
+    {
+        question: "Under which HTML tag can you write the Javascript code?",
+        choices: ["<javascript>", "<scripts>", "<js>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        question: "Which of the following is the correct syntax to display 'Hello World' in an alert box using Javascript?",
+        choices: ["alert('Hello World')", "prompt('Hello World')", "alertbox('Hello World')", "alertuser('Hello World')"],
+        answer: "alert('Hello World')"
+    },
+    {
+        question: "Which of the following is true about Javascript?",
+        choices: ["Javascript is an object-oriented programming lanugage.", "Javascript is a dynamic computer programming language.", "Javascript is a scripting language.", "All of the above."],
+        answer: "All of the above."
+    },
+    {
+        question: "What is the different between '==' and '==='?",
+        choices: ["They are the same.", "'==' checks for equality in values, while '===' checks for quality and value and type of variable.", "'===' is only used for Booleans.", "None of the above."],
+        answer: "'==' checks for equality in values, while '===' checks for quality and value and type of variable."
+    },
+    {
+        question: "What method should one use to create a new HTML element by the specified tagname?",
+        choices: ["document.createElement()", "Node.createElement()", "create.newElement()", "create.TextNode()"],
+        answer: "document.createElement()"
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    // },
+    // {
+    //     question: a,
+    //     choices: a,
+    //     answer: a,
+    }
+];
