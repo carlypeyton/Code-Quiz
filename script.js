11be177801af9a1f7207964d3bdcb64c689beeb4
Carly Peyton <carlypeyton@gmail.com>
2020-12-21 11:56:57 -0500

    Add script file. Define variables and assign variables to DOM.

---
+++ script.js
@@ -0,0 +1,13 @@
+/////////////////////////////QUIZ HTML///////////////////////////////////
+//Define variables
+var countdown = 60;
+var timerInterval;
+var quiz;
+var quizEnd;
+var questions;
+
+//Assign variables for DOM
+var timeLeft = document.querySelector("#time-left");
+var quizBody = document.querySelector(".quiz-body");
+var quizIntro = document.querySelector(".quiz-intro");
+
